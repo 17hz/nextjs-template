5a4c40a95b6679ebfb405dd8ed919c234acefd26
17hz <0x149527@gmail.com>
2025-11-01 20:33:21 +0800

    chore: upgrade dependencies and update project configuration
    - Upgrade Next.js to 16.0.1, React to 19.2.0, ESLint to 9.38.0
    - Migrate ESLint config to flat config format and add style plugins
    - Enable React Compiler and update TypeScript/PostCSS config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,36 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import eslintPluginBetterTailwindcss from "eslint-plugin-better-tailwindcss";
+import stylistic from '@stylistic/eslint-plugin'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
-];
+    plugins:{
+      '@stylistic': stylistic,
+      "better-tailwindcss": eslintPluginBetterTailwindcss,
+    },
+    settings: {
+      "better-tailwindcss": {
+        // tailwindcss 4: the path to the entry file of the css based tailwind config (eg: `src/global.css`)
+        "entryPoint": "src/app/globals.css",
+      }
+    },
+    rules: {
+      '@stylistic/indent': ['error', 2],
+      'better-tailwindcss/enforce-consistent-class-order': 'error',
+    }
+  }
+]);
 
 export default eslintConfig;
