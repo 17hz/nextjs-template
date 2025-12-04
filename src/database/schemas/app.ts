08b4000d420e3505825758d7d4e0067dfcaa6693
17hz <0x149527@gmail.com>
2025-12-04 15:07:09 +0800

    feat:  better-auth integration

--- src/database/schemas/app.ts
+++ src/database/schemas/app.ts
@@ -1,6 +1,6 @@
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
-export const appTable = pgTable("apps", {
+export const app = pgTable("apps", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   version: varchar({ length: 255 }).notNull(),
